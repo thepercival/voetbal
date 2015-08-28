e6b6859f434fcf70fe9446fe5e186409b1a31aa4
thepercival <coendunnink@gmail.com>
2015-08-28 09:23:41 +0200

    added package

---
+++ README.md
@@ -0,0 +1,5 @@
+# Laravel 5 Module voor voetbal
+
+## Omschrijving
+
+Er zijn views en een biblitheek aanwezig om allerlei zaken met voetbal te kunnen doen. Onder andere competities, spelers, standen etc. beheren.
---
+++ src/ServiceProvider.php
@@ -0,0 +1,79 @@
+<?php namespace Voetbal;
+
+use Illuminate\Support\ServiceProvider as LaravelServiceProvider;
+
+class ServiceProvider extends LaravelServiceProvider {
+
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot() {
+
+        $this->handleConfigs();
+        // $this->handleMigrations();
+        // $this->handleViews();
+        // $this->handleTranslations();
+        // $this->handleRoutes();
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register() {
+
+        // Bind any implementations.
+
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides() {
+
+        return [];
+    }
+
+    private function handleConfigs() {
+
+        $configPath = __DIR__ . '/../config/voetbal.php';
+
+        $this->publishes([$configPath => config_path('voetbal.php')]);
+
+        $this->mergeConfigFrom($configPath, 'voetbal');
+    }
+
+    private function handleTranslations() {
+
+        $this->loadTranslationsFrom('voetbal', __DIR__.'/../lang');
+    }
+
+    private function handleViews() {
+
+        $this->loadViewsFrom('voetbal', __DIR__.'/../views');
+
+        $this->publishes([__DIR__.'/../views' => base_path('resources/views/vendor/voetbal')]);
+    }
+
+    private function handleMigrations() {
+
+        $this->publishes([__DIR__ . '/../migrations' => base_path('database/migrations')]);
+    }
+
+    private function handleRoutes() {
+
+        include __DIR__.'/../routes.php';
+    }
+}
