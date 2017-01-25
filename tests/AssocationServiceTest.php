eaf83d35ecd2bb677416482ef23403c972bc69cf
thepercival <git30903>
2017-01-25 21:57:58 +0100

    update voetbal

---
+++ tests/AssocationServiceTest.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: coen
+ * Date: 25-1-17
+ * Time: 21:50
+ */
+
+namespace Voetbal\Tests;
+
+use Voetbal\Association as Association;
+use Voetbal\Repository\Association as AssociationRepository;
+
+class AssociationServiceTest extends \PHPUnit_Framework_TestCase
+{
+	public function testCreateNameMin()
+	{
+		$em = null;
+		$associationRepos = new AssociationRepository( $em );
+		$service = new Association\Service( $associationRepos );
+		// $this->expectException(\InvalidArgumentException::class);
+		// $associationName = new Association\Name("");
+	}
+}
