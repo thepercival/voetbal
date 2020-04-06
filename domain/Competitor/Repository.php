a6e458614392f13636ce698e8bf583eb56046397
thepercival <coendunnink@gmail.com>
2020-04-06 16:37:08 +0200

    update lockerrooms

--- domain/Competitor/Repository.php
+++ domain/Competitor/Repository.php
@@ -8,7 +8,11 @@
 
 namespace Voetbal\Competitor;
 
+use Voetbal\Association;
+use Voetbal\Competition;
+use Voetbal\Competitor;
 use Voetbal\Competitor as CompetitorBase;
+use Voetbal\Game as GameBase;
 
 /**
  * Class Repository
@@ -19,5 +23,33 @@ class Repository extends \Voetbal\Repository
     public function find($id, $lockMode = null, $lockVersion = null): ?CompetitorBase
     {
         return $this->_em->find($this->_entityName, $id, $lockMode, $lockVersion);
+    }
+
+    public function removeUnused( Association $association )
+    {
+        $queryBuilder = $this->createQueryBuilder('c')
+            ->delete('Voetbal\Competitor', 'c')
+            ->where('c.association = :association')
+            ->andWhere(
+                $this->getEM()->getExpressionBuilder()->not(
+                    $this->getEM()->getExpressionBuilder()->exists(
+                        $this->getEM()->createQueryBuilder()
+                            ->select('pp.id')
+                            ->from('Voetbal\Place', 'pp')
+                            ->join("pp.poule", "p")
+                            ->join("p.round", "r")
+                            ->join("r.number", "rn")
+                            ->join("rn.competition", "comp")
+                            ->join("comp.league", "l")
+                            ->where('l.association = c.association')
+                            ->andWhere('pp.competitor = c')
+                            ->getDQL()
+                    )
+                )
+            )
+            ->setParameter('association', $association)
+        ;
+
+        $queryBuilder->getQuery()->execute();
     }
 }
--- domain/Game/Repository.php
+++ domain/Game/Repository.php
@@ -62,64 +62,57 @@ class Repository extends \Voetbal\Repository
         return count($x) === 1;
     }
 
-    /**
-     * @param Competitor $homeCompetitor
-     * @param Competitor $awayCompetitor
-     * @param Competition $competition
-     * @param int|null $gameStates
-     * @return mixed| GameBase[]
-     * @throws \Exception
-     */
-    public function findByExt( Competitor $homeCompetitor, Competitor $awayCompetitor, Competition $competition, int $gameStates = null)
-    {
-        $exprHome = $this->getEM()->getExpressionBuilder();
-        $exprAway = $this->getEM()->getExpressionBuilder();
 
-        $query = $this->createQueryBuilder('g')
-            ->join("g.poule", "p")
-            ->join("p.round", "r")
-            ->join("r.number", "rn")
-            ->where('rn.competition = :competition')
-            ->andWhere(
-                $exprHome->exists(
-                    $this->getEM()->createQueryBuilder()
-                        ->select('gpphome.id')
-                        ->from('Voetbal\Game\PoulePlace', 'gpphome')
-                        ->join("gpphome.poulePlace", "pphome")
-                        ->where('gpphome.game = g')
-                        ->andWhere('gpphome.homeaway = :home' )
-                        ->andWhere('pphome.competitor = :homecompetitor')
-                        ->getDQL()
-                )
-            )
-            ->andWhere(
-                $exprAway->exists(
-                    $this->getEM()->createQueryBuilder()
-                        ->select('gppaway.id')
-                        ->from('Voetbal\Game\PoulePlace', 'gppaway')
-                        ->join("gppaway.poulePlace", "ppaway")
-                        ->where('gppaway.game = g')
-                        ->andWhere('gppaway.homeaway = :away')
-                        ->andWhere('ppaway.competitor = :awaycompetitor')
-                        ->getDQL()
-                )
-            )
-        ;
-        if( $gameStates !== null ) {
-
-            $query = $query->andWhere('BIT_AND(g.state, :gamestates) = g.state');
-            // $query = $query->andWhere('(g.state & :gamestates) = g.state');
-        }
-        $query = $query->setParameter('competition', $competition);
-        $query = $query->setParameter('home', GameBase::HOME);
-        $query = $query->setParameter('homecompetitor', $homeCompetitor);
-        $query = $query->setParameter('away', GameBase::AWAY);
-        $query = $query->setParameter('awaycompetitor', $awayCompetitor);
-        if( $gameStates !== null ) {
-            $query = $query->setParameter('gamestates', $gameStates);
-        }
-        return $query->getQuery()->getResult();
-    }
+//    public function findByExt( Competitor $homeCompetitor, Competitor $awayCompetitor, Competition $competition, int $gameStates = null)
+//    {
+//        $exprHome = $this->getEM()->getExpressionBuilder();
+//        $exprAway = $this->getEM()->getExpressionBuilder();
+//
+//        $query = $this->createQueryBuilder('g')
+//            ->join("g.poule", "p")
+//            ->join("p.round", "r")
+//            ->join("r.number", "rn")
+//            ->where('rn.competition = :competition')
+//            ->andWhere(
+//                $exprHome->exists(
+//                    $this->getEM()->createQueryBuilder()
+//                        ->select('gpphome.id')
+//                        ->from('Voetbal\Game\Place', 'gpphome')
+//                        ->join("gpphome.poulePlace", "pphome")
+//                        ->where('gpphome.game = g')
+//                        ->andWhere('gpphome.homeaway = :home' )
+//                        ->andWhere('pphome.competitor = :homecompetitor')
+//                        ->getDQL()
+//                )
+//            )
+//            ->andWhere(
+//                $exprAway->exists(
+//                    $this->getEM()->createQueryBuilder()
+//                        ->select('gppaway.id')
+//                        ->from('Voetbal\Game\PoulePlace', 'gppaway')
+//                        ->join("gppaway.poulePlace", "ppaway")
+//                        ->where('gppaway.game = g')
+//                        ->andWhere('gppaway.homeaway = :away')
+//                        ->andWhere('ppaway.competitor = :awaycompetitor')
+//                        ->getDQL()
+//                )
+//            )
+//        ;
+//        if( $gameStates !== null ) {
+//
+//            $query = $query->andWhere('BIT_AND(g.state, :gamestates) = g.state');
+//            // $query = $query->andWhere('(g.state & :gamestates) = g.state');
+//        }
+//        $query = $query->setParameter('competition', $competition);
+//        $query = $query->setParameter('home', GameBase::HOME);
+//        $query = $query->setParameter('homecompetitor', $homeCompetitor);
+//        $query = $query->setParameter('away', GameBase::AWAY);
+//        $query = $query->setParameter('awaycompetitor', $awayCompetitor);
+//        if( $gameStates !== null ) {
+//            $query = $query->setParameter('gamestates', $gameStates);
+//        }
+//        return $query->getQuery()->getResult();
+//    }
 
     /**
      * @param GameBase $game
